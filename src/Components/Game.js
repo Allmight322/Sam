7aadf8122476a67eadebe597daccf7c1119773ee
Игорь <ogigora@mail.ru>
2022-07-01 19:46:59 +0500

    Новый проект

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,24 +1,49 @@
 import classes from './Game.module.css'
 const Game=()=>{
    return(
-       <div className={classes.style}>
+       <div className={classes.main}>
+           <div>
+               <table>
+                   <tr>
+                       <td className={classes.main2}>1</td>
+                       <td className={classes.main2}>2</td>
+                       <td className={classes.main2}>3</td>
+                       <td className={classes.main2}>4</td>
+                       <td className={classes.main2}>5</td>
+                       <td className={classes.main2}>6</td>
+                   </tr>
+               </table>
+           </div>
         <table>
             <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
+                <td className={classes.style}>1</td>
+                <td className={classes.style}>2</td>
+                <td className={classes.style}>3</td>
              </tr>
             <tr>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
+                <td className={classes.style}>4</td>
+                <td className={classes.style}>5</td>
+                <td className={classes.style}>6</td>
              </tr>
             <tr>
-                <td>7</td>
-                 <td>8</td>
-                 <td>9</td>
+                 <td className={classes.style}>7</td>
+                 <td className={classes.style}>8</td>
+                 <td className={classes.style}>9</td>
              </tr>
          </table>
+           <div>
+               <table>
+                   <tr>
+                       <td className={classes.main2}>1</td>
+                       <td className={classes.main2}>2</td>
+                       <td className={classes.main2}>3</td>
+                       <td className={classes.main2}>4</td>
+                       <td className={classes.main2}>5</td>
+                       <td className={classes.main2}>6</td>
+                   </tr>
+               </table>
+           </div>
+
        </div>
    )
 };
