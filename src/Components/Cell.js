61a49fc1e7c742d2a290e90e1b3b52fcfdbd51c8
alwertus <alwertus@gmail.com>
2022-07-27 01:08:36 +0300

    Функция смены местоположения фигуры

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -5,35 +5,25 @@ const Cell = (props) => {
     const Number = props.Number
     const Group = props.Group
     const Figures = props.Figures ? props.Figures : []
-    const Selected = props.Selected
-    // const SetFiguresAtr = props.SetFiguresAtr
-
+    const changeFigurePosition = props.changeFigurePosition
 
     let figure
-
 
     for (let i = 0; i < Figures.length; i++) {
         if (Figures[i].props.Number === Number && Figures[i].props.Group === Group)
             figure = Figures[i]
     }
-    const drawElement = () => {
-        if (Selected === undefined)
-            return
-        for (let i = 0; i < Figures.length; i++) {
-            if (Figures[i].props.id === Selected) {
-                Figures[i].props.Number = Number
-                Figures[i].props.Group = Group
 
-
-            }
-
-
-        }
-        return (
-            <div onClick={drawElement} className={classes.Cell}>{figure}</div>
-
-        )
+    const onCellClick = () => {
+        changeFigurePosition(Group, Number)
     }
+
+    return (
+        <div onClick={onCellClick} className={classes.Cell}>
+            {figure}
+        </div>
+    )
+
 }
 
 export default Cell;
