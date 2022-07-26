57b2d8692807bd19f1a1d7dd705399cc1e86b53f
Игорь <ogigora@mail.ru>
2022-07-26 16:57:21 +0500

    Новый проект

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -6,15 +6,36 @@ const Cell=(props)=>{
    const Number=props.Number
    const Group = props.Group
    const Figures = props.Figures ? props.Figures: []
+   const Selected = props.Selected
+   const SetFiguresAtr = props.SetFiguresAtr
+
 
    let figure
+
 
    for (let i = 0; i < Figures.length; i++) {
       if (Figures[i].props.Number===Number && Figures[i].props.Group===Group)
          figure=Figures[i]
    }
+   const drawElement=()=>{
+      if (Selected===undefined)
+         return
+      for (let i = 0; i < Figures.length; i++) {
+         if (Figures[i].props.id === Selected){
+         Figures[i].props.Number=Number
+            Figures[i].props.Group=Group
+
+
+
+      }
+
+
+
+
+   }
    return(
-       <div className={classes.Cell}>{figure}</div>
+       <div onClick={drawElement} className={classes.Cell}>{figure}</div>
+
    )
 };
 
--- src/Components/Game.js
+++ src/Components/Game.js
@@ -71,36 +71,47 @@ l-16 -56 -80 -34 c-118 -50 -181 -94 -280 -198 -97 -103 -127 -152 -127 -209
 
 const Game=()=>{
     const [selected, setSelected]= useState()
+    const [figuresAtr, setFiguresAtr]=useState([
+        {id:'1', Number:'1', Group:'1', Size:'1', TeamNumber:'1'},
+        {id:'2', Number:'2', Group:'1', Size:'1', TeamNumber:'1'},
+        {id:'3', Number:'3', Group:'1', Size:'2', TeamNumber:'1'},
+        {id:'4', Number:'4', Group:'1', Size:'2', TeamNumber:'1'},
+        {id:'5', Number:'5', Group:'1', Size:'3', TeamNumber:'1'},
+        {id:'6', Number:'6', Group:'1', Size:'3', TeamNumber:'1'},
+        {id:'7', Number:'1', Group:'3', Size:'1', TeamNumber:'2'},
+        {id:'8', Number:'2', Group:'3', Size:'1', TeamNumber:'2'},
+        {id:'9', Number:'3', Group:'3', Size:'2', TeamNumber:'2'},
+        {id:'10', Number:'4', Group:'3', Size:'2', TeamNumber:'2'},
+        {id:'11', Number:'5', Group:'3', Size:'3', TeamNumber:'2'},
+        {id:'12', Number:'6', Group:'3', Size:'3', TeamNumber:'2'},
+    ])
+    const handleChange = e => {
+
+    }
+    const figures=figuresAtr.map(e=><Figure id={e.id} Number={e.Number}  Group={e.Group}  Size={e.Size}  TeamNumber={e.TeamNumber}  Image={image} Selected={selected} SetSelected={setSelected}/>)
 
 
-    const figures = [
-        <Figure id='1' Number='1' Group='1' Size='1' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='2'  Number='2' Group='1' Size='1' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='3'  Number='3' Group='1' Size='2' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='4'  Number='4' Group='1' Size='2' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='5'  Number='5' Group='1' Size='3' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='6'  Number='6' Group='1' Size='3' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='7'  Number='1' Group='3' Size='1' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='8'  Number='2' Group='3' Size='1' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='9'  Number='3' Group='3' Size='2' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='10'  Number='4' Group='3' Size='2' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='11'  Number='5' Group='3' Size='3' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-        <Figure id='12'  Number='6' Group='3' Size='3' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/>,
-    ]
+
+const getCell=(number, group, style='')=>
+    <td className={style}><Cell Number={number} Group={group} Figures={figures} Selected={selected} SetFiguresAtr={setFiguresAtr}/></td>
+
+
 
 
     console.log(selected)
+    console.log(figures)
     return(
        <div className={classes.main}>
            <div >
               <table>
                    <tr>
-                       <td><Cell Number='1' Group='1' Figures={figures} /></td>
-                       <td><Cell Number='2' Group='1' Figures={figures}  /></td>
-                       <td><Cell Number='3' Group='1' Figures={figures} /></td>
-                       <td><Cell Number='4' Group='1' Figures={figures} /></td>
-                       <td><Cell Number='5' Group='1' Figures={figures} /></td>
-                       <td><Cell Number='6' Group='1' Figures={figures}/></td>
+                       {getCell('1','1')}
+                       {getCell('2','1')}
+                       {getCell('3','1')}
+                       {getCell('4','1')}
+                       {getCell('5','1')}
+                       {getCell('6','1')}
+
                      {/*<td><Figure Number='1' Size='1' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/> </td>
                        <td><Figure Number='2' Size='1' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/></td>
                        <td><Figure Number='3' Size='2' TeamNumber='1' Image={image} Selected={selected} SetSelected={setSelected}/></td>
@@ -112,30 +123,31 @@ const Game=()=>{
            </div>
         <table >
             <tr>
-                <td className={classes.style}><Cell Number='1' Group='2'/></td>
-                <td className={classes.style}><Cell Number='2' Group='2'/></td>
-                <td className={classes.style}><Cell Number='3' Group='2'/></td>
+                {getCell('1','2',classes.style)}
+                {getCell('2','2',classes.style)}
+                {getCell('3','2',classes.style)}
+
              </tr>
             <tr>
-                <td className={classes.style}><Cell Number='4' Group='2'/></td>
-                <td className={classes.style}><Cell Number='5' Group='2'/></td>
-                <td className={classes.style}><Cell Number='6' Group='2'/></td>
+                {getCell('4','2',classes.style)}
+                {getCell('5','2',classes.style)}
+                {getCell('6','2',classes.style)}
              </tr>
             <tr>
-                 <td className={classes.style}><Cell Number='7' Group='2'/></td>
-                 <td className={classes.style}><Cell Number='8' Group='2'/></td>
-                 <td className={classes.style}><Cell Number='9' Group='2'/></td>
+                {getCell('7','2',classes.style)}
+                {getCell('8','2',classes.style)}
+                {getCell('9','2',classes.style)}
              </tr>
          </table>
            <div>
                <table>
                    <tr>
-                       <td><Cell Number='1' Group='3' Figures={figures} /></td>
-                       <td><Cell Number='2' Group='3' Figures={figures} /></td>
-                       <td><Cell Number='3' Group='3' Figures={figures}/></td>
-                       <td><Cell Number='4' Group='3' Figures={figures} /></td>
-                       <td><Cell Number='5' Group='3' Figures={figures} /></td>
-                       <td><Cell Number='6' Group='3' Figures={figures} /></td>
+                       {getCell('1','3')}
+                       {getCell('2','3')}
+                       {getCell('3','3')}
+                       {getCell('4','3')}
+                       {getCell('5','3')}
+                       {getCell('6','3')}
                       {/* <td><Figure Number='7' Size='1' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/></td>
                        <td><Figure Number='8' Size='1' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/></td>
                        <td><Figure Number='9' Size='2' TeamNumber='2' Image={image} Selected={selected} SetSelected={setSelected}/></td>
